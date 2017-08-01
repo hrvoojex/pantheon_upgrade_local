f11b4b0426454f6dc8e90235d125285a3e120e81
hrvooje <hrvooje@gmail.com>
2017-08-01 12:54:34 +0200

    Added progress bar, centered hyperlink. Progress bar is not working with pyqtSignal. It sets value to progresbarr via callback method

--- pantheon_upgrade_local.py
+++ pantheon_upgrade_local.py
@@ -19,18 +19,16 @@ Ako je procesor 32-bitni onda je 'processor=i686'
 Ovdje je objašnjeno --> url: https://pymotw.com/2/platform/
 """
 
-import platform
-import sys
-from shutil import copy
-from PyQt5 import QtWidgets, QtCore
+import sys, os, platform
+from PyQt5 import QtWidgets, QtCore, QtGui
 from design2 import Ui_Dialog  # import from my 'design2.py' module
-from PyQt5.QtCore import QUrl
-from PyQt5.QtGui import QDesktopServices
-from PyQt5.QtGui import QIcon
 
 
 class Main(QtWidgets.QWidget):
     """The main widget with label and LineEdit."""
+
+    # Create a signal
+    #copied_percent_signal = QtCore.pyqtSignal(int)
 
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -54,11 +52,13 @@ class Main(QtWidgets.QWidget):
         # Open a link in a default browser
         self.ui.label_2.linkActivated.connect(self.link)
         self.ui.label_2.setText(
-            '<a href="https://drive.google.com/drive/folders/0B_fNrhELg9mKSnFyU0JIQVNqZmc">Download link - Pantheon.exe</a>')
+            '<a href="https://drive.google.com/drive/folders/'
+            '0B_fNrhELg9mKSnFyU0JIQVNqZmc">Download link - Pantheon.exe</a>')
+        self.ui.label_2.setAlignment(QtCore.Qt.AlignCenter)
 
     def link(self, linkStr):
 
-        QDesktopServices.openUrl(QUrl(linkStr))
+        QtGui.QDesktopServices.openUrl(QtCore.QUrl(linkStr))
 
     def selectfile_Dialog(self, event=None):
         """
@@ -100,16 +100,36 @@ class Main(QtWidgets.QWidget):
 
         return(my_pantheon_path)
 
-    def saving(self):
-        my_pantheon_path = self.where_to_save()
-        copy(self.ui.lineEdit.text(), my_pantheon_path)
-        print("Finished succesfully.")
-
     def pushButtonAction(self):
         """Press the left click on pushButton."""
-        self.where_to_save()
-        self.saving()
-        self.ui.label_3.setText("Kopiranje završeno uspješno!")
+        self.ui.pushButton.setDisabled(True)
+        self.src_file = self.ui.lineEdit.text()
+        self.dst_file = self.where_to_save()
+        self.file_size = os.stat(self.src_file).st_size
+        self.copyfileobj(self.src_file, self.dst_file, self.my_callback)
+        #self.copied_percent_signal.connect(self.on_count_change)
+        print("Finished succesfully.")
+        self.ui.label_3.setText("Nadogradna uspješno završena!")
+
+    def on_count_change(self, value):
+        self.ui.progressBar.setValue(value)
+
+    def my_callback(self, temp_file_size):
+        percent = int(temp_file_size/self.file_size*100)
+        print("Copiedd: {}".format(percent))
+        #self.copied_percent_signal.emit(percent)
+        self.ui.progressBar.setValue(percent)
+
+    def copyfileobj(self, fsrc, fdst, callback, length=16*1024):
+        copied = 0
+        with open(fsrc, "rb") as fr, open(fdst, "wb") as fw:
+            while True:
+                buff = fr.read(length)
+                if not buff:
+                    break
+                fw.write(buff)
+                copied += len(buff)
+                callback(copied)
 
 
 class ClickableLineEdit(QtWidgets.QLineEdit):
@@ -145,26 +165,26 @@ class MyApp(QtWidgets.QMainWindow):
     def initUi(self):
         """Initialize UI of an application."""
         # main window size, title
-        self.setGeometry(400, 300, 400, 400)
+        self.setGeometry(400, 300, 400, 525)
         self.setWindowTitle("Pantheon - nadogradnja verzije ")
-        self.setWindowIcon(QIcon('pan_ico.png'))
+        self.setWindowIcon(QtGui.QIcon('pan_ico.png'))
 
         # Create central widget and set is as centra widget
         self.centralWidget = Main(self)
         self.setCentralWidget(self.centralWidget)
 
         # Create menubar with actions
-        exitAction = QtWidgets.QAction(QIcon('exit.png'), '&Exit', self)
+        exitAction = QtWidgets.QAction(QtGui.QIcon('exit.png'), '&Exit', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Zatvori aplikaciju')
         exitAction.triggered.connect(QtWidgets.qApp.quit)
 
-        aboutAction = QtWidgets.QAction(QIcon('about.png'), 'About', self)
+        aboutAction = QtWidgets.QAction(QtGui.QIcon('about.png'), 'About', self)
         aboutAction.setShortcut('Ctrl+B')
         aboutAction.setStatusTip('O aplikaciji')
         aboutAction.triggered.connect(self.showAbout)
 
-        programAction = QtWidgets.QAction(QIcon('program.png'), 'Program', self)
+        programAction = QtWidgets.QAction(QtGui.QIcon('program.png'), 'Program', self)
         programAction.setShortcut('Ctrl+P')
         programAction.setStatusTip('Pokreni program')
         programAction.triggered.connect(self.showMain)
